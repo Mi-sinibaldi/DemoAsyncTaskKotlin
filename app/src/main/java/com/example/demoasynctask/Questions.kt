c92b7f8297974aadabbd91b8eb863162674486b8
Michelle <mia_araujo92@hotmail.com>
2019-08-10 13:51:48 -0300

    Initial commit

---
+++ app/src/main/java/com/example/demoasynctask/Questions.kt
@@ -0,0 +1,10 @@
+package com.example.demoasynctask
+
+class Questions {
+    var Question: String = ""
+    var Option1: String = ""
+    var Option2: String = ""
+    var Option3: String = ""
+    var Option4: String = ""
+    var Answer: Int = 0
+}
